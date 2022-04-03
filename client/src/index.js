4c3b0d6c55b2da0bf0438dece91b9295876d9b1b
Muhammad <tahiratta27@gmail.com>
2022-04-03 23:23:29 +0500

    First Commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import '../node_modules/hover.css/css/hover-min.css';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
