b147e33031d09eedcd43f2786098754319ae59f3
Muhammad <tahiratta27@gmail.com>
2022-04-03 22:45:55 +0500

    First Commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/hover.css/css/hover-min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/hover.css/css/hover-min.css';
 import './index.css';
 import App from './App';
 
